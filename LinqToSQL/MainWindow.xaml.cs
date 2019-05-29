2465b8863b323e51df770f5830a86085709ab8e8
Gavin Quirk <gavinquirk@gmail.com>
2019-05-29 13:33:08 -0700

    add universities, add students

--- LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/MainWindow.xaml.cs
@@ -30,6 +30,51 @@ namespace LinqToSQL
 
             string connectionString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.gqdbConnectionString"].ConnectionString;
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+
+            //InsertUniversities();
+            InsertStudents();
+        }
+
+        // Add a University
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University yale = new University(); // New instance of University
+            yale.Name = "Yale"; // Set name property of new instance
+            dataContext.Universities.InsertOnSubmit(yale); // Insert data into collection on submit
+
+            University harvard = new University();
+            harvard.Name = "Harvard";
+            dataContext.Universities.InsertOnSubmit(harvard);
+
+            dataContext.SubmitChanges(); // Submit
+
+            MainDataGrid.ItemsSource = dataContext.Universities; // Set source of data for MainDataGrid element to Universities collection
+        }
+
+        // Add Students
+        public void InsertStudents()
+        {
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale")); // First document in University collection which has the name Yale. Return as object
+            University harvard = dataContext.Universities.First(un => un.Name.Equals("Harvard"));
+
+            List<Student> students = new List<Student>(); // Initialize a new list of Students
+
+            students.Add(new Student { Name = "Carla", Gender = "female", UniversityId = yale.Id }); // Add using university Id
+            students.Add(new Student { Name = "Toni", Gender = "male", University = yale }); // Add using university object
+            students.Add(new Student { Name = "Leyla", Gender = "female", University = harvard });
+            students.Add(new Student { Name = "James", Gender = "trans-gender", University = harvard });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+
+
+
+
+
         }
     }
 }
